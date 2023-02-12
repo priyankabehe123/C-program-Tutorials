a09aa930a68517d9bcba7243b5e0a3adb341c1cb
priyankabehe123 <124875318+priyankabehe123@users.noreply.github.com>
2023-02-12 18:05:15 +0530

    monika

---
+++ monika.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("priyanka");
+	printf("\n\t monika");
+	printf("\n\t\t kanchana");
+	printf("\n\t\t\t purnnachandra");
+	printf("\n\t\t\t\t monisha");
+	return 0;
+}
