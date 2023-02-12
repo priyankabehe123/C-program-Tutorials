7456381e692833cef97fbe09e376028cb740887e
priyankabehe123 <124875318+priyankabehe123@users.noreply.github.com>
2023-02-12 22:52:59 +0530

    addtion of 2 numbers

---
+++ addition1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,sum;
+	printf("enter the value of x:");
+	scanf("%d",&x);
+	printf("enter the value of y:");
+	scanf("%d",&y);
+	sum=x+y;
+	printf("after addition of x and y is:%d",x+y);
+}
